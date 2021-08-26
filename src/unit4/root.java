a0e6e00bf8555e721c12ba79681c31be7e47fb5a
alsignat <alsignat@gmail.com>
2021-08-27 00:43:09 +0300

    root func

---
+++ src/unit4/root.java
@@ -0,0 +1,11 @@
+package unit4;
+
+public class root {
+    public static void main(String[] args) {
+        int number = 225;
+        System.out.println("Квадрный корень из "
+                + number
+                + " равен "
+                + Math.sqrt(number));
+    }
+}
