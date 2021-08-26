9f7e3b49cceba606b7e1cb70c2ea15d92a37698c
alsignat <alsignat@gmail.com>
2021-08-26 23:53:39 +0300

    Initial commit

---
+++ src/unit2/SalutonMondo.java
@@ -0,0 +1,8 @@
+package unit2;
+
+public class SalutonMondo {
+    public static void main (String [] args) {
+        String greeting = "Saluton Mondo!";
+        System.out.println(greeting);
+    }
+}
